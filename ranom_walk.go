bf4c20f99f9f945f60217613a1ef0886aa149598
Michael Thaler <michael.thaler@dlr.de>
2025-08-31 11:06:33 +0200

    Updated random_walk.go.

--- ranom_walk.go
+++ ranom_walk.go
@@ -1,5 +1,7 @@
 package main
 
+import "math/rand/v2"
+
 type position struct {
 	x float64
 	y float64
@@ -8,8 +10,20 @@ type position struct {
 func generateRandomWalk2D(x, y float64, steps int) []position {
 	var positions []position
 	positions = make([]position, 0)
-	xx := x
-	yy := y
+	stepX := 0.0
+	if rand.Float64() >= 0.5 {
+		stepX = 1.0
+	} else {
+		stepX = -1.0
+	}
+	stepY := 0.0
+	if rand.Float64() >= 0.5 {
+		stepY = 1.0
+	} else {
+		stepY = -1.0
+	}
+	xx := x + stepX
+	yy := y + stepY
 	p := position{x: xx, y: yy}
 	positions = append(positions, p)
 	return positions
