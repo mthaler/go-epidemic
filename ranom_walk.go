e054e2ef6e15d3a796a750e6ed0f6de62f0676e8
Michael Thaler <michael.thaler@dlr.de>
2025-08-31 11:51:12 +0200

    Updated ranom_walk.go.

--- ranom_walk.go
+++ ranom_walk.go
@@ -7,24 +7,18 @@ type position struct {
 	y float64
 }
 
-func generateRandomWalk2D(x, y float64, steps int) []position {
-	var positions []position
-	positions = make([]position, 0)
-	stepX := 0.0
+func step(p position) position {
+	x := p.x
+	y := p.y
 	if rand.Float64() >= 0.5 {
-		stepX = 1.0
+		x += 1.0
 	} else {
-		stepX = -1.0
+		x -= 1.0
 	}
-	stepY := 0.0
 	if rand.Float64() >= 0.5 {
-		stepY = 1.0
+		x += 1.0
 	} else {
-		stepY = -1.0
+		x -= 1.0
 	}
-	xx := x + stepX
-	yy := y + stepY
-	p := position{x: xx, y: yy}
-	positions = append(positions, p)
-	return positions
+	return position{x: x, y: y}
 }
