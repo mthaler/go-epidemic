a88b3597bb2f349f3a2b98fb8ed2242eac1b00a3
Michael Thaler <michael.thaler@dlr.de>
2025-08-31 10:37:55 +0200

    Updated random_walk.go.

--- ranom_walk.go
+++ ranom_walk.go
@@ -8,5 +8,9 @@ type position struct {
 func generateRandomWalk2D(x, y float64, steps int) []position {
 	var positions []position
 	positions = make([]position, 0)
+	xx := x
+	yy := y
+	p := position{x: xx, y: yy}
+	positions = append(positions, p)
 	return positions
 }
